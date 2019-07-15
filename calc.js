0a1f8e692cd7eac1faf09a06141a32d16f42a450
nunustolemybike <remskonieczny@gmail.com>
2019-07-15 14:43:07 +0200

    ajout de calc.js

---
+++ calc.js
@@ -0,0 +1,21 @@
+module.exports= {
+ add:function(x,y) {
+x=x+y
+return x
+},
+
+ substract:function(x,y){
+x=x-y
+return x
+},
+
+  multiply:function(x,y){
+x=x*y
+return x
+},
+
+  divide:function(x,y){
+x=x/y
+return x
+}
+};
